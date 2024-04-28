5b7a0e17ff7261e7d65a2e08afade3c361a83cdd
aoumad <abderazzakoumad@gmail.com>
2024-04-28 16:12:44 +0100

    implemented a mini_webserv using net/http package and another one using GIN framework

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+	r.Run(":8080")
+}
+
+func handler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello World",
+	})
+}
